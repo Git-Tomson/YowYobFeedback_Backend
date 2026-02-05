836838c290e410add27e0667033f5096bf5e4383
Override <tstdtomson@gmail.com>
2026-02-05 16:44:12 +0100

    Make public the endpoint of health cheick

--- src/main/java/com/yowyob/feedback/config/SecurityConfig.java
+++ src/main/java/com/yowyob/feedback/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     private static final String SWAGGER_UI_PATH_PATTERN = "/swagger-ui/";
     private static final String SWAGGER_HTML_PATH = "/swagger-ui.html";
     private static final String ACTUATOR_PATH_PATTERN = "/actuator/";
+    private static final String HEALTH_CHEICK_PATTERN = "/api/v1/health";
 
     private final SecurityContextRepository security_context_repository;
 
@@ -67,6 +68,7 @@ public class SecurityConfig {
                         .pathMatchers(API_DOCS_PATH_PATTERN + "**", SWAGGER_UI_PATH_PATTERN + "**",
                                 SWAGGER_HTML_PATH).permitAll()
                         .pathMatchers(ACTUATOR_PATH_PATTERN + "**").permitAll()
+                        .pathMatchers(HEALTH_CHEICK_PATTERN).permitAll()
                         .anyExchange().authenticated()
                 )
                 .securityContextRepository(security_context_repository)
